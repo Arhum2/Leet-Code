e05b19a4dd903b68e4c693b40d3163ab88e3d1cf
arhum2 <arhum.shahzad2003@gmail.com>
2024-05-09 00:00:12 -0400

    Arhum: worked on another solution

--- Arrays and hashing/duplicate.py
+++ Arrays and hashing/duplicate.py
@@ -11,16 +11,32 @@ from typing import List
 
 # too slow ^
     
-class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
+# class Solution:
+#     def containsDuplicate(self, nums: List[int]) -> bool:
 
-        dict = {}
+#         dict = {}
         
-        for num in nums:
-            if num not in dict:
-                dict[num] = 0
-            else:
-                return True
-        return False
+#         for num in nums:
+#             if num not in dict:
+#                 dict[num] = 0
+#             else:
+#                 return True
+#         return False
 
 # used a hashmap lol
+
+def containsDuplicate(nums: List[int]) -> bool:
+    for num in nums:
+        temp_num = nums.remove(num)
+        if num in nums:
+            return True
+    return False
+
+#run this code on [1,2,1]
+
+if __name__=="__main__":
+    # s = Solution()
+    containsDuplicate([274,-735,-911,80,454,-511,922,-775,985,-669,-463,-896,-629,-586,910,-361,288,-375,88,556,-578,-406,-87,25,377,-635,-445,-289,646,-962,-487,-924,-968,-962,502,36,129,-611,54,-27,-496,915,-84,-782,349,678,332,-114,345,14,119,710,821,-194,988,38,-369,409,-559,-529,-298,-593,705])
+
+# Crappier solution
+# the reason this solution fails sometimes is because the for loop skips a value when it removes a value from the list
